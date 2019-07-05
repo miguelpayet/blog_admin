56c23372d9bedcc97dc141ca99f0a350225f25fe
miguel <miguelpayet@gmail.com>
2019-07-04 22:32:57 -0500

    home

--- blog_admin/urls.py
+++ blog_admin/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from app.views import admin_index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', admin_index, name="index"),
 ]
