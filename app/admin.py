5401c11ab13a558322985710af72ad8146f44232
miguel <miguelpayet@gmail.com>
2019-07-07 15:52:20 -0500

    entry

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.forms import ModelChoiceField
+from django.forms import ModelForm
+from django.forms import RadioSelect
 
-from app.models import Entry, Tag, TagFoto, Foto
+from app.models import Entry
+from app.models import Foto
+from app.models import Tag
+from app.models import TagFoto
+from app.models import Tipo
+
+
+class TipoAdmin(admin.ModelAdmin):
+    list_display = ('idtipo', 'nombre',)
+    fields = ('idtipo', 'nombre',)
+
+
+admin.site.register(Tipo, TipoAdmin)
 
 
 class TagInline(admin.TabularInline):
@@ -9,7 +24,21 @@ class TagInline(admin.TabularInline):
     extra = 0
 
 
+class EntryForm(ModelForm):
+    tipo = ModelChoiceField(
+        initial=Tipo.BLOG,
+        label='Tipo',
+        queryset=Tipo.objects.all(),
+        widget=RadioSelect(attrs={'class': 'inline'}),
+    )
+
+    class Meta:
+        model = Entry
+        fields = ('titulo', 'handle', 'descripcion', 'tipo',)
+
+
 class EntryAdmin(admin.ModelAdmin):
+    form = EntryForm
     list_display = ('identry', 'titulo', 'tags',)
     inlines = (TagInline,)
 
